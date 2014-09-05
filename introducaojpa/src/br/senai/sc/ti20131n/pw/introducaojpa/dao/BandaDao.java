d0721b2c4cf94bfa67b2a7ca1ead6e417729c2f6
bernardo_broba <bernardo_broba@SNFNS2EP13VM056.florianopolis.sc.senai.br>
2014-09-04 21:54:09 -0300

    ADD teste

---
+++ introducaojpa/src/br/senai/sc/ti20131n/pw/introducaojpa/dao/BandaDao.java
@@ -0,0 +1,18 @@
+package br.senai.sc.ti20131n.pw.introducaojpa.dao;
+
+import javax.persistence.EntityManager;
+
+import br.senai.sc.ti20131n.pw.introducaojpa.entity.Banda;
+
+public class BandaDao {
+	private EntityManager entityManager;
+	
+	public BandaDao(EntityManager entityManager){
+		this.entityManager = entityManager;
+				
+	}
+
+	public void salvar(Banda banda){
+		entityManager.persist(banda);
+	}
+}
---
+++ introducaojpa/test/br/senai/sc/ti20131n/pw/introducaojpa/dao/BandaDaoTest.java
@@ -0,0 +1,54 @@
+package br.senai.sc.ti20131n.pw.introducaojpa.dao;
+
+import javax.persistence.EntityManager;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import br.senai.sc.ti20131n.pw.introducaojpa.entity.Banda;
+import br.senai.sc.ti20131n.pw.introducaojpa.util.JpaUtil;
+
+public class BandaDaoTest  {
+public static EntityManager entityManager;
+	
+	@BeforeClass
+	public static void init(){
+		JpaUtil.initFactory();
+		
+	}
+	 
+	@AfterClass
+	public static void finish(){
+		JpaUtil.closeFactory();
+		
+	}
+
+  @Before
+  public void begin(){
+	  entityManager = JpaUtil.getEntityManager();
+	  entityManager.getTransaction().begin();
+
+	  
+  }
+  @After
+  public void close(){
+	  entityManager.getTransaction().commit();
+	  entityManager.close();
+	  entityManager = null;
+	  
+  }
+  @Test
+  public void testSalvar(){
+	  Banda banda = new Banda();
+	  banda.setNome("banda 11");
+	  banda.setHistoria("blablabla");
+	  
+	  BandaDao dao = new BandaDao(entityManager);
+	  dao.salvar(banda);
+  }
+  
+}
+
